ca4f6c7936f0114f87cf023c399a4725bc201819
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-02-15 12:41:40 -0300

    feat: player dtos

---
+++ backend/src/infra/controller/dto/players/create-player.dto.ts
@@ -0,0 +1,5 @@
+export class CreatePlayerDTO {
+  name: string;
+  age: number;
+  teamId: string;
+}
---
+++ backend/src/infra/controller/dto/players/update-player.dto.ts
@@ -0,0 +1,6 @@
+export class UpdatePlayerDTO {
+  name?: string;
+  age?: number;
+  teamId?: string;
+  id: string;
+}
